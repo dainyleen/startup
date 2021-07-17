89c10fcfaf59930798874d6cd00b20a5c1db775d
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-17 19:24:54 +1200

    Added modal skeleton structure to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,28 @@ function displayEmployeeGallery(employeeInfo) {
 $('.gallery').on('click', '.card', function(event) {
   employeeNumber = parseInt(event.currentTarget.getAttribute('id') - 1)
   const modalGallery = document.getElementById('gallery')
+  employeeInfo.then(data => {
+    const employee = data.results[employeeNumber]
+
+    // Employee information will be displayed in the modal view
+    const modalHTML = `
+    <div class="modal-container">
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src=${employee.picture.large} alt="profile picture">
+        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+        <p class="modal-text">${employee.email}</p>
+        <p class="modal-text cap">${employee.location.city}</p>
+        <hr>
+        <p class="modal-text">Mobile: ${employee.cell}</p>
+        <p class="modal-text">Address: ${employee.location.street.number} ${employee.location.street.name}, ${employee.location.country}, ${employee.location.postcode}</p>
+        <p class="modal-text">Birthday: ${employee.dob.date}</p>
+      </div>
+    </div>
+    </div>`
+    modalGallery.insertAdjacentHTML('beforeend', modalHTML)
+  })
 })
 
 
