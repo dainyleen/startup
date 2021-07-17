63cc260ecd23916be3b0d051a5e4e0a99476403e
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-17 18:33:09 +1200

    Added a for loop inside the function to get all 12 employees

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,21 @@ function displayEmployeeGallery(employeeInfo) {
   const gallery = document.getElementById('gallery')
   // Loop through the employee info data
   employeeInfo.then(data => {
-    for (let i = 0; i < data.results.length) {
+    for (let i = 0; i < data.results.length; i++) {
+      const profileImage = data.results[i].picture.medium
+      const name = data.results[i].name
+      const email = data.results[i].email
+      const location = data.results[i].location
 
+      // Add HTML to the gallery to be displayed
+      const html = `
+      <div>
+      <div>
+      </div>
+      <div>
+      </div>
+      </div>
+      `
     }
   })
 }
