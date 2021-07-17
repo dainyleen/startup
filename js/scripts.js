5fa00a81d9d654cd3f294e3812ac93e69b81e1c3
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-17 18:25:50 +1200

    Started creating displayEmployeeGallery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-// API for returning 12 random users
-url = 'https://randomuser.me/api/?results=12&nat=nz'
+/***********
+ FETCH CALL
+***********/
 
+url = 'https://randomuser.me/api/?results=12&nat=nz'
+const employeeInfo = fetchEmployees(url)
+displayEmployeeGallery(employeeInfo)
+
+/****************
+ HELPER FUNCTION
+****************/
+
+function displayEmployeeGallery(employeeInfo) {
+  const gallery = document.getElementById('gallery')
+  // Loop through the employee info data
+  employeeInfo.then(data => {
+    for (let i = 0; i < data.results.length) {
+
+    }
+  })
+}
 
